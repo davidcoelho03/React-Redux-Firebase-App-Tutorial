4b034542a483cdbef9f857672e8e15b441eb5c93
David Coelho <44527632+davidcoelho03@users.noreply.github.com>
2019-04-08 17:39:16 +0100

    lesson no 12 - Adding Dummy Data: - Fixed some reducers exporting name issues - Added some dummy data to the projectReducer - Mapped the state from store to Dashboard props and sent it to ProjectList - Modified ProjectList to receive the projects as props and mapped   through them, returning a jsx for each project - Modified ProjectSummary to receive the project prop and show it

--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -1,9 +1,13 @@
 const initState = {
-
+    projects: [
+        {id: '1', title: 'help me finding nemo', content: 'tengo la camisa negra'},
+        {id: '2', title: 'found nemo', content: 'tengo la camisa negra'},
+        {id: '3', title: 'it wasnt nemo', content: 'tengo la camisa negra'}
+    ]
 }
 
 const projectReducer = (state = initState, action) => {
     return state;
 }
 
-export default projectReducer();
+export default projectReducer;
