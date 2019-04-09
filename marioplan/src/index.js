d132ca03e4df60ffed1396b972e3936971943cbc
David Coelho <44527632+davidcoelho03@users.noreply.github.com>
2019-04-09 10:23:16 +0100

    lesson no 17 - Connecting Redux to Firebase - Imported redux-firestore and react-redux-firebase - Used compose to connect the firebase config with the project actions

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import fbConfig from './config/fbConfig'
 
 // applying middleware to the store to increase store's functionality. In this case fetching data async
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
+// thunk -> store enhancer
+const store = createStore(rootReducer,
+    compose(
+        applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+        reduxFirestore(fbConfig),
+        reactReduxFirebase(fbConfig)
+    )
+);
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
