73835da899bedb017857636007c9f4fe78a39b9a
David Coelho <44527632+davidcoelho03@users.noreply.github.com>
2019-04-10 11:01:16 +0100

    lesson no 35 - Notification Component - Updated Notification Component - Removed some logs from console

--- marioplan/src/components/layout/Navbar.js
+++ marioplan/src/components/layout/Navbar.js
@@ -5,7 +5,6 @@ import SignedOutLinks from './SignedOutLinks'
 import { connect } from 'react-redux'
 
 const Navbar = (props) => {
-    console.log(props);
     const { auth, profile } = props;
     const links = auth.uid ? <SignedInLinks profile={profile}/> : <SignedOutLinks/>;
     return (
@@ -19,7 +18,6 @@ const Navbar = (props) => {
 }
 
 const mapStateToProps = (state) => {
-    console.log(state);
     return {
         auth: state.firebase.auth,
         profile: state.firebase.profile
