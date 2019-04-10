7799c1a3561fdd397fcef65714ea05e25a0ebb6d
David Coelho <44527632+davidcoelho03@users.noreply.github.com>
2019-04-10 13:58:26 +0100

    lesson no 39 - Ordering Firestore Data - ordered notifications elements on the Dashboard

--- marioplan/src/components/dashboard/Dashboard.js
+++ marioplan/src/components/dashboard/Dashboard.js
@@ -7,39 +7,39 @@ import { compose } from 'redux'
 import { Redirect } from 'react-router-dom'
 
 class Dashboard extends Component {
-    render(){
-        // console.log(this.props.projects);
-        const { projects, auth, notifications } = this.props;
-        // checking if there's anyone logged in
-        if(!auth.uid) return <Redirect to='/signin' />
+   render(){
+      // console.log(this.props.projects);
+      const { projects, auth, notifications } = this.props;
+      // checking if there's anyone logged in
+      if(!auth.uid) return <Redirect to='/signin' />
 
-        return (
-            <div className="dashboard container">
-                <div className="row">
-                    <div className="col s12 m6">
-                        <ProjectList projects={projects} />
-                    </div>
-                    <div className="col s12 m5 offset-m1">
-                        <Notifications notifications={notifications}/>
-                    </div>
-                </div>
+      return (
+         <div className="dashboard container">
+            <div className="row">
+               <div className="col s12 m6">
+                  <ProjectList projects={projects} />
+               </div>
+               <div className="col s12 m5 offset-m1">
+                  <Notifications notifications={notifications}/>
+               </div>
             </div>
-        )
-    }
+         </div>
+      )
+   }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        projects: state.firestore.ordered.projects,
-        auth: state.firebase.auth,
-        notifications: state.firestore.ordered.notifications
-    }
+   return {
+      projects: state.firestore.ordered.projects,
+      auth: state.firebase.auth,
+      notifications: state.firestore.ordered.notifications
+   }
 };
 
 export default compose(
-    connect(mapStateToProps),
-    firestoreConnect([
-        { collection: 'projects' },
-        { collection: 'notifications', limit: 3}
-    ])
+   connect(mapStateToProps),
+   firestoreConnect([
+      { collection: 'projects', orderBy: ['createdAt', 'desc']},
+      { collection: 'notifications', limit: 3, orderBy: ['time', 'desc']}
+   ])
 )(Dashboard);
